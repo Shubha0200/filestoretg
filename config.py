7c0751f0b373bd366e214af68454934c3a8317d7
Shubha0200 <rb5028432@gmail.com>
2025-02-07 12:16:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,38 +1,42 @@
-limport os
+import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7474955516:AAG6hNIVmwFYtZwi1WW0H3d5GX7Ihh4gQbQ")
-API_ID = int(os.environ.get("API_ID", "21459238"))
-API_HASH = os.environ.get("API_HASH", "0cef1924d139587654bc18402d2b512d")
+# Fetch environment variables (remove sensitive data from default values)
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "1782088532"))
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://shubhashankar246:nEWTylzLZpiuysRh@cluster0.kpecd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "shubhashankar246")
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+DB_URL = os.environ.get("DB_URL")
+DB_NAME = os.environ.get("DB_NAME")
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002448726887"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002343164262"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL"))
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600"))  # auto delete in seconds
+FILE_AUTO_DELETE = int(os.environ.get("FILE_AUTO_DELETE", "600"))  # auto delete in seconds
 PORT = os.environ.get("PORT", "8080")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-ADMINS = []
-for x in os.environ.get("ADMINS", "1782088532").split():
-    ADMINS.append(int(x))
+# Ensure ADMINS is correctly populated from environment variables
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "1782088532").split()]
 
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION")
 
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
-DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
+# Convert PROTECT_CONTENT and DISABLE_CHANNEL_BUTTON to booleans
+PROTECT_CONTENT = os.environ.get('PROTECT_CONTENT', "True") == "True"
+DISABLE_CHANNEL_BUTTON = os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True"
 
+# Messages
 BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
 START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
 
+# Log file name
 LOG_FILE_NAME = "filesharingbot.txt"
 
+# Setup logging configuration
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -46,6 +50,7 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
